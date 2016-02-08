a8e1edc26cbf71bf3e413f8b94da9d9244384d19
manojbraj <manojbraj@outlook.com>
2016-02-08 19:23:13 +0530

    Manoj : My Activity Plus SpecoMeter Design

---
+++ app/src/main/java/com/h5c/careengage/jsoninterface/SpeedChangeListener.java
@@ -0,0 +1,8 @@
+package com.h5c.careengage.jsoninterface;
+
+/**
+ * Created by user on 2/8/2016.
+ */
+public interface SpeedChangeListener {
+    public void onSpeedChanged(float newSpeedValue);
+}
